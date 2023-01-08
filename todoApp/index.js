b2af533c2141bc5aadc99bc74829d50483a9d224
arviyonismara <arviyonismara@gmail.com>
2023-01-08 23:08:43 +0700

    edit todo.js

--- todoApp/index.js
+++ todoApp/index.js
@@ -10,73 +10,15 @@ const todoInput = document.querySelector(".todo-input");
 const btnInput = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-const KEY_TODOS = "Td_qw";
-
-function renderTodo(){
-  const todos = JSON.parse(localStorage.getItem(KEY_TODOS));
-  todoList.innerHTML = "";
-
-  todos.forEach(todo => {
-    // membuat div
-    const div = document.createElement("div");
-    // menambahkan class ke div
-    div.classList.add("todo");
-
-    // membuat li
-    const li = document.createElement("li");
-    // manambahkan class ke li
-    li.classList.add("todo-item");
-    // masukkan todoInput ke li
-    li.innerText = todo;
-
-    // mebuat done btn
-    const done = document.createElement("button");
-    // menambahkan class ke button
-    done.classList.add("done-btn");
-    // menambahkan text Done ke button
-    done.innerText = "Done";
-
-    // membuat delete btn
-    const del = document.createElement("button");
-    // menambahkan class ke button
-    del.classList.add("delete-btn");
-    // menambahkan text Delete ke button
-    del.innerText = "Delete";
-
-    // masukkan li, done dan del ke dalam div
-    div.append(li, done, del);
-
-    // masukkan div ke dalam todoList
-    todoList.append(div);
-
-    // kosongkan input
-    todoInput.value = "";
-  });
-}
-
-let newTodos = [];
 // event listener
 btnInput.addEventListener("click", (event) => {
   event.preventDefault();
-  const todos = JSON.parse(localStorage.getItem(KEY_TODOS));
 
   // check jika inputnya kosong, maka beri alert
-  
-  // }
-  // check jika todos belum ada
-  if (todos === null) {
-    newTodos.push(todoInput.value);
-  }
-  else if (todoInput.value === "") {
+  if (todoInput.value === "") {
     alert("Input tidak boleh kosong");
     return;
   }
-  else {
-    newTodos = [...todos, todoInput.value];
-  }
-  localStorage.setItem(KEY_TODOS, JSON.stringify(newTodos));
-  renderTodo();
-
 
   // membuat element todo.
   /*
@@ -87,39 +29,39 @@ btnInput.addEventListener("click", (event) => {
     </div>
   */
 
-  // // membuat div
-  // const div = document.createElement("div");
-  // // menambahkan class ke div
-  // div.classList.add("todo");
+  // membuat div
+  const div = document.createElement("div");
+  // menambahkan class ke div
+  div.classList.add("todo");
 
-  // // membuat li
-  // const li = document.createElement("li");
-  // // manambahkan class ke li
-  // li.classList.add("todo-item");
-  // // masukkan todoInput ke li
-  // li.innerText = todoInput.value;
+  // membuat li
+  const li = document.createElement("li");
+  // manambahkan class ke li
+  li.classList.add("todo-item");
+  // masukkan todoInput ke li
+  li.innerText = todoInput.value;
 
-  // // mebuat done btn
-  // const done = document.createElement("button");
-  // // menambahkan class ke button
-  // done.classList.add("done-btn");
-  // // menambahkan text Done ke button
-  // done.innerText = "Done";
+  // mebuat done btn
+  const done = document.createElement("button");
+  // menambahkan class ke button
+  done.classList.add("done-btn");
+  // menambahkan text Done ke button
+  done.innerText = "Done";
 
-  // // membuat delete btn
-  // const del = document.createElement("button");
-  // // menambahkan class ke button
-  // del.classList.add("delete-btn");
-  // // menambahkan text Delete ke button
-  // del.innerText = "Delete";
+  // membuat delete btn
+  const del = document.createElement("button");
+  // menambahkan class ke button
+  del.classList.add("delete-btn");
+  // menambahkan text Delete ke button
+  del.innerText = "Delete";
 
-  // // masukkan li, done dan del ke dalam div
-  // div.append(li, done, del);
+  // masukkan li, done dan del ke dalam div
+  div.append(li, done, del);
 
-  // // masukkan div ke dalam todoList
-  // todoList.append(div);
+  // masukkan div ke dalam todoList
+  todoList.append(div);
 
-  // // kosongkan input
+  // kosongkan input
   todoInput.value = "";
 });
 
@@ -144,11 +86,6 @@ todoList.addEventListener("click", (event) => {
     // yg di click tombol delete
     // tambahkan class remove ke todo
     todo.classList.add("remove");
-    // for(let i = 0; i < newTodos.length; i++){
-    //   if(li.innerText == newTodos[i]){
-    //     continue;
-    //   }
-    // }
 
     // hapus todo menggunakan transitionend
     todo.addEventListener("transitionend", () => {
